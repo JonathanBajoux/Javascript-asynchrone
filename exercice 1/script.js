2285dd0fcad61655660b0171a021ad6869b2f56d
jonathan <jonathanbajoux.pro@gmail.com>
2022-12-19 12:31:33 +0100

    Joanthan

--- exercice 1/script.js
+++ exercice 1/script.js
@@ -1,7 +1,12 @@
+//creer une var qui selectionne la balise html
 let button = document.querySelector(".click");
+
+//ajouter un event au bouton avec une fonction
 button.addEventListener("click", ()=> fetchFile());
 
+//creer une balise 
 let ul = document.createElement('ul');
+//appeler la balise
 document.body.appendChild(ul);
 
 
