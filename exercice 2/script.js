bb32a58822ca5e90cd065bd40fc1fb91a64817d5
jonathan <jonathanbajoux.pro@gmail.com>
2022-12-19 17:02:12 +0100

    jonathan

--- exercice 2/script.js
+++ exercice 2/script.js
@@ -1,26 +1,26 @@
+let testObject = [];
+
 function searchAge() {
     let searchName = document.getElementById('name').value;
     let searchCountry = document.getElementById('country').value;
 
     const fetchName = (name, country) => fetch("https://api.agify.io/?name=" + name + "&country_id=" + country);
 
-    fetchName (searchName, searchCountry)
-.then((response) => response.json())
-.then((json)=> {
-    let addDiv = document.createElement("div");
-    addDiv.innerText= searchName + ", poeple with the same name are " + json.age + " years old and there are " + json.count;
-    document.body.appendChild(addDiv);
-    })
-    .catch((error) =>{
-        console.log(error);
-    });
+    fetchName(searchName, searchCountry)
+        .then((response) => response.json())
+        .then((json) => {
+            let addDiv = document.createElement("div");
+            addDiv.innerText = searchName + ", poeple with the same name are " + json.age + " years old and there are " + json.count;
+            testObject.push(addDiv.innerText);
+            localStorage.setItem('save', JSON.stringify(testObject));
+            document.body.appendChild(addDiv);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 document.querySelector('.press1').addEventListener("click", searchAge)
 
-let testObject = {};
+testObject = JSON.parse(localStorage.getItem('save'));
 
-localStorage.setItem('testObject', JSON.stringify(testObject));
-
-let retrievedObject = localStorage.getItem('testObject');
-
-console.log('retrievedObject:', JSON.parse(retrievedObject));
+console.log(testObject);
